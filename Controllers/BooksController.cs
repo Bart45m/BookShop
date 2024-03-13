b739fddfbe92d8ab1f1bea2c53f5afa654d01411
Bart45m <bartoszkopciuch8@gmail.com>
2024-03-13 15:19:10 +0100

    create migration, seed and basic controllers with services

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rental.Data;
+using Rental.Models;
+using Rental.Services;
+using Rental.ViewModels;
+
+namespace Rental.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+        private readonly RentalDbContext _context;
+
+        public BooksController(IBookService bookService, RentalDbContext context)
+        {
+            _bookService = bookService;
+            _context = context;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookViewModel>>> GetBooks()
+        {
+            try
+            {
+                var books = await _bookService.GetBooks();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST: api/Books
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<BookViewModel>> PostBook(BookViewModel book)
+        {
+            try { 
+                await _bookService.PostBook(book);
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                return Problem("Entity set 'RentalDbContext.Books'  is null.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Rental.Data;
+using Rental.Controllers;
+using System.Text.Json.Serialization;
+using Rental.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<RentalDbContext>(
+    options => options.UseSqlite("name=ConnectionStrings:DatabaseConnection")
+);
+
+builder.Services.AddScoped<IBookService, BookService>();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
